0d9e7a7824c53abf722c0e565d32d1837f9da176
fesan <Kukskalle231086>
2020-03-25 12:13:42 +0100

    0.2

--- freq.py
+++ freq.py
@@ -29,20 +29,17 @@ f = ["5hz", "10hz", "11hz", "20hz", "53hz", "100hz", "190hz", "253hz", "350hz", 
 
 writeDB = "UPDATE freq({}) VALUES({}) WHERE id={}".format(f, v, dbid)
 xi = 0
-for x in f:
 
+for x in f:
+	input("Set frequency to {}").format(x)
 	for d in range(30):
 		sleep(1)
 		v[d] = infreq
 		infreq = 0
 
-#	print(v)
-#	print(xi)
-#	print(f[xi])
-
 	for i in range(30):
 		# db_insert(f[x], v[i], i)
 		print(x, v[i], i)
 
-	x =+ 1
-	input("Change frequency")
+	# x =+ 1
+	# input("Change frequency")
