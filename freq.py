6e016cdb23d3769166fc62d83b320dd850817723
fesan <Kukskalle231086>
2020-03-25 13:11:13 +0100

    0.2

--- freq.py
+++ freq.py
@@ -29,7 +29,7 @@ x = 0
 
 f = ["5hz", "10hz", "11hz", "20hz", "53hz", "100hz", "190hz", "253hz", "350hz", "500hz", "600hz", "700hz", "800hz", "900hz", "1000hz", "1500hz", "3000hz", "6000hz", "9000hz", "12000hz", "15000hz", "18000hz", "21000hz"]
 
-writeDB = "UPDATE freq SET {} = {} WHERE id = {}".format(f[x], v, dbid)
+writeDB = "UPDATE freq SET {} = {} WHERE id = {}".format(x, v, i2)
 
 print("      Raspberry Pi - Frequency test")
 print("        Written by Stefan Bahrawy")
@@ -52,7 +52,7 @@ for x in f:
 	print("Reading done, now DB insert")
 	for i in range(30):
 		i2 = i + 1
-		print(x, v[i], i)
+		print(x, v[i], i2)
 		db_insert(x, v[i], i2)
 		input("pause")
 	print("\x1b[1;32m 30 readings stored in database column {} \x1b[0m".format(x))
