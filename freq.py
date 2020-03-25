66c683ca8952a18a112d6fb274c9edd8e2704b0d
fesan <Kukskalle231086>
2020-03-25 13:00:20 +0100

    0.2

--- freq.py
+++ freq.py
@@ -48,10 +48,11 @@ for x in f:
 		sleep(1)
 		v[d] = infreq
 		infreq = 0
-
+	print("Reading done, now DB insert")
 	for i in range(30):
-		db_insert(x, v[i], i)
-		# print(x, v[i], i)
+		# db_insert(x, v[i], i)
+		print(x, v[i], i)
+		input("pause")
 	print("\x1b[1;32m 30 readings stored in database column {} \x1b[0m".format(x))
 	print("")
 
