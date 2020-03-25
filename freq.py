ddaf38e46b761f0adbfdece072fcf360af5328ea
Stefan Bahrawy <stefan@bahrawy.net>
2020-03-25 10:44:07 +0000

    0.2

--- freq.py
+++ freq.py
@@ -2,13 +2,13 @@ import mysql.connector
 import RPi.GPIO as g
 from time import sleep
 
-global input
-input = 0
+global infreq
+infreq = 0
 
 
 def freq(x):
-	global input
-	input = + 1
+	global infreq
+	infreq = + 1
 
 
 def db_insert(f, v, dbid):
@@ -28,17 +28,20 @@ v = [0] * 30
 f = ["5hz", "10hz", "11hz", "20hz", "53hz", "100hz", "190hz", "253hz", "350hz", "500hz", "600hz", "700hz", "800hz", "900hz", "1000hz", "1500hz", "3000hz", "6000hz", "9000hz", "12000hz", "15000hz", "18000hz", "21000hz"]
 
 writeDB = "UPDATE freq({}) VALUES({}) WHERE id={}".format(f, v, dbid)
-
+xi = 0
 for x in f:
 
 	for d in range(30):
-		sleep(0.1)
-		v[d] = input
-	
-	print(v)
+		sleep(1)
+		v[d] = infreq
+
+#	print(v)
+#	print(xi)
+#	print(f[xi])
 
 	for i in range(30):
 		# db_insert(f[x], v[i], i)
-		print(f[x])
+		print(x, v[i], i)
 
+	x =+ 1
 	input("Change frequency")
